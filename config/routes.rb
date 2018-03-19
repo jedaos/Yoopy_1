a16afd4bb54bbdb8e1b23c8b7f5fc6fee5495677
Your Name <dre.murguido@gmail.com>
2018-03-19 11:28:56 -0400

    created hospitals and professionals

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
+  # devise_for :profs
+  # devise_for :hospitals
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "home#index"
+  devise_for :hospitals, path: 'hospitals', controllers: { sessions: "hospitals/sessions" }
+  devise_for :profs, path: 'profs', controllers: { sessions: "profs/sessions" }
 end
