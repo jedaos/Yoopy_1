bd5ce00d0567f3a7f44af7c9a364e0f46259b1b9
Alex Evans <aeboston1@gmail.com>
2018-03-20 15:41:00 -0400

    signup forms now saving to db

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:institution_name, :street,
+    :email, :phone, :city, :zip, :license_num, :job_title, :bio, :image ])
+    #in keys you list all the input you want to accept.
+  end
 end
