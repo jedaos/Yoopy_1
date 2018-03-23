1d59a284c9ae7d037f64476dfb9d66dcf924b5ac
Alex Evans <aeboston1@gmail.com>
2018-03-23 10:23:03 -0400

    dashboard views and stripe update

--- app/assets/javascripts/components/no_edit_job.es6.jsx
+++ app/assets/javascripts/components/no_edit_job.es6.jsx
@@ -8,6 +8,7 @@ class NoEditJob extends React.Component {
          <li><h5>Description: {this.props.job.description}</h5></li>
          <li><h5>Rate: {this.props.job.rate}</h5></li>
          <li><h5>Slots: {this.props.job.slot_num}</h5></li>
+         
        </ul>
      </div>
 
