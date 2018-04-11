e8523f8216bc261fdc444f84122b4d3238a09022
Your Name <dre.murguido@gmail.com>
2018-04-11 16:01:10 -0400

    resolved conflicts

--- app/assets/javascripts/components/friends/current_friend_jobs_wrapper.es6.jsx
+++ app/assets/javascripts/components/friends/current_friend_jobs_wrapper.es6.jsx
@@ -2,13 +2,8 @@ class CurrentFriendJobsWrapper extends React.Component {
 
   render () {
     return (
-<<<<<<< HEAD:app/assets/javascripts/components/friends/current_friend_jobs_wrapper.es6.jsx
           <div className="container" id="FriendJobList">
             <h3 className="align-left">Your Current Jobs</h3>
-=======
-          <div className="">
-            <h5 className="align-left">Your Current Jobs</h5>
->>>>>>> eda3519a45711814cf428397881993166cf222e2:app/assets/javascripts/components/current_friend_jobs_wrapper.es6.jsx
             {this.props.friendJobs.map((x) => {
             return <FriendJob key={x.id} job={x} />
             })}
--- app/assets/javascripts/components/friends/new_friend_job.es6.jsx
+++ app/assets/javascripts/components/friends/new_friend_job.es6.jsx
@@ -74,13 +74,8 @@ class NewFriendJob extends React.Component {
                 <input
                   type='text'
                   onChange={this.handleTitleChange}
-<<<<<<< HEAD:app/assets/javascripts/components/friends/new_friend_job.es6.jsx
                   value={job.name}
                   placeholder="Title"
-=======
-                  value={this.state.name}
-                  placeholder="Job Title"
->>>>>>> eda3519a45711814cf428397881993166cf222e2:app/assets/javascripts/components/new_friend_job.es6.jsx
                   >
                 </input>
               </div>
@@ -89,13 +84,8 @@ class NewFriendJob extends React.Component {
             <div className="right-align">
               <div className="input-field col s6">
                 <div className="col s6">
-<<<<<<< HEAD:app/assets/javascripts/components/friends/new_friend_job.es6.jsx
                   <select className="browser-default" value={job.rate} onChange={this.handleRateChange}>
                     <option value="Rate" className="disabled">Rate</option>
-=======
-                  <select className="browser-default" value={this.state.rate} onChange={this.handleRateChange}>
-                    <option value="Rate" className="disabled">Select Rate</option>
->>>>>>> eda3519a45711814cf428397881993166cf222e2:app/assets/javascripts/components/new_friend_job.es6.jsx
                     <option value="100">$100</option>
                     <option value="200">$200</option>
                     <option value="300">$300</option>
@@ -110,13 +100,8 @@ class NewFriendJob extends React.Component {
                     id="description"
                     type='text'
                     onChange={this.handleDescriptionChange}
-<<<<<<< HEAD:app/assets/javascripts/components/friends/new_friend_job.es6.jsx
                     value={job.description}
                     placeholder="Description"
-=======
-                    value={this.state.description}
-                    placeholder="Tell us what type of care you're looking for"
->>>>>>> eda3519a45711814cf428397881993166cf222e2:app/assets/javascripts/components/new_friend_job.es6.jsx
                     >
                   </textarea>
                   </div>
@@ -126,7 +111,7 @@ class NewFriendJob extends React.Component {
 
           <div className="row">
             <div className="col s">
-              <input className="btn-large green-button" type="submit" value="Submit"></input>
+              <input className="btn btn-primary" type="submit" value="Submit"></input>
             </div>
           </div>
         </form>
