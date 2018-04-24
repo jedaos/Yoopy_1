a4803ebfb61e1ea55a5ef81f81fdf030dcca3575
Alex Evans <aeboston1@gmail.com>
2018-04-24 13:52:11 -0400

    fixed html inside friend job card

--- app/assets/javascripts/components/friends/new_friend_job.es6.jsx
+++ app/assets/javascripts/components/friends/new_friend_job.es6.jsx
@@ -86,8 +86,8 @@ class NewFriendJob extends React.Component {
     <div className="" id="friendJobForm">
       <div className="row">
         <form className="col s12" onSubmit={this.handleSubmit}>
-          <div className="row">
 
+          <div className="row">
             <div>
               <div className="input-field col s12">
                 <input
@@ -101,80 +101,47 @@ class NewFriendJob extends React.Component {
             </div>
           </div>
 
-          <div className="row">
-
-                  <div className="col s6">
-                      <select className="browser-default" value={job.rate} onChange={this.handleRateChange}>
-                        <option value="Rate" className="disabled">Rate</option>
-                        <option value="100">$100</option>
-                        <option value="200">$200</option>
-                        <option value="300">$300</option>
-                      </select>
-                    </div>
-
-                  <div className="col s6">
-                      <select className='browser-default' value={job.slot_num} onChange={this.handleSlotChange}>
-                        <option value="Slots" className="disabled"> Number of Professionals</option>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                      </select>
-                  </div>
-
-<<<<<<< HEAD
-            <div>
-              <div className="input-field col s6">
-                <div className="">
-                  <select className="browser-default" value={job.rate} onChange={this.handleRateChange}>
-                    <option value="Rate" className="disabled">Rate</option>
-                    <option value="100">$100</option>
-                    <option value="200">$200</option>
-                    <option value="300">$300</option>
-                  </select>
-                </div>
-              </div>
-            </div>
-
-              <div>
-                <div className="input-field col s12">
-=======
-              </div>
-
-            <div className="col s12">
-              <div className="row">
-                <div className="input-field">
->>>>>>> 8a603b84532fa3ca768838d5fbbc24a8cd46b887
-                  <textarea
-                    id="description"
-                    type='text'
-                    onChange={this.handleDescriptionChange}
-                    value={job.description}
-                    placeholder="Add a description to tell us about the type of medical professional you're looking for"
-                    >
-                  </textarea>
-                </div>
-              </div>
-<<<<<<< HEAD
-=======
-
-
-          <div className="row">
-            <div className="col s1">
-              <input className="btn btn-primary" type="submit" value="Submit"></input>
->>>>>>> 8a603b84532fa3ca768838d5fbbc24a8cd46b887
-            </div>
-            <input className="btn-large green-button" type="submit" value="Post Job"></input>
-
-            </form>
+        <div className="row">
+          <div className="col s8">
+            <select className='browser-default' value={job.slot_num} onChange={this.handleSlotChange}>
+              <option value="Slots" className="disabled">Number of Professionals</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+            </select>
           </div>
-<<<<<<< HEAD
         </div>
-=======
+
+        <div className="row">
+          <div className="input-field col s8">
+            <div className="">
+              <select className="browser-default" value={job.rate} onChange={this.handleRateChange}>
+                <option value="Rate" className="disabled">Rate</option>
+                <option value="100">$100</option>
+                <option value="200">$200</option>
+                <option value="300">$300</option>
+              </select>
+            </div>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="input-field">
+            <textarea
+              id="description"
+              type='text'
+              onChange={this.handleDescriptionChange}
+              value={job.description}
+              placeholder="Add a description to tell us about the type of medical professional you're looking for">
+            </textarea>
+          </div>
+        </div>
+
+        <input className="btn-large green-button" type="submit" value="Post Job"></input>
+
         </form>
       </div>
-
-
->>>>>>> 8a603b84532fa3ca768838d5fbbc24a8cd46b887
+    </div>
     );
   }
 }
