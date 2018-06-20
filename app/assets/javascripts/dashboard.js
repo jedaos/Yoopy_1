0762e2d015ce2232432eaae581ac1ab8859f0869
Your Name <dre.murguido@gmail.com>
2018-06-20 16:16:08 -0400

    professionals phone numbers are being texted if they are within the radius

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -182,25 +182,31 @@
       }
     });
   })
+
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    let currentLocation = {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude
+    }
+    if (currentLocation){
+      window.GlobalVar = currentLocation
+      // initMap(currentLocation);
+    }
+  })
   // Sending Text after friendJob created
-  function computeDistance(address_lat_lng){
-
-      var marker_lat_lng = new google.maps.LatLng(gon.profs[1].lat, gon.profs[1].lng);
-      var address = new google.maps.LatLng(address_lat_lng.lat, address_lat_lng.lng);
-      var distance_from_location = google.maps.geometry.spherical.computeDistanceBetween(address, marker_lat_lng);
-      if (distance_from_location <= 1609.34) {
-         console.log(distance_from_location);
-      }
-
- }
-  // 1 mile in meters = 1609.34
   function text() {
       let form = $(".new_job_form");
+      // 1 mile in meters = 1609.34
+      let radius = Number($("input[name='miles']:checked").val()) * 1609.34;
+      console.log(radius);
+      let prof_numbers = computeDistance(radius, window.GlobalVar);
+      console.log(prof_numbers);
       form.submit(function(){
-        console.log("submitted");
         $.ajax({
           url: "notifications/notify_text",
           method: "POST",
+          data: { profs: prof_numbers },
           success: (res) => {
             console.log("Success, message sent", res);
           },
@@ -210,36 +216,42 @@
         })
       })
     }
-  //Google maps
-  function initMap(){
-    opts = {
-      enableHighAccuracy: true
-    }
-    function success(pos){
-     let currentLoc = {
-       lat: pos.coords.latitude,
-       lng: pos.coords.longitude
-     }
-     computeDistance(currentLoc)
-     let map1;
-     map1 = new google.maps.Map(document.getElementById('map1'), {
-       center: new google.maps.LatLng(currentLoc),
-       zoom: 10
-     });
-     let markers;
-     if (gon.friendBook){
-       gon.friendBook.map(loc => {
-        locations = {
-           lat: loc.lat,
-           lng: loc.lng
-         }
-         markers = new google.maps.Marker({ position: locations, map: map1 })
-       })
-     }
-    }
-    function failure(err){
-     console.log(err);
-   }
+    //Compute distance between professionals and current location and store the numbers into an array.
+  function computeDistance(radius, currentLocation){
+    let prof_addresses = []
+    let prof_phones = []
+    var address = new google.maps.LatLng(currentLocation.lat, currentLocation.lng);
+      for (var i = 0; i < gon.profs.length; i++) {
+        var marker_lat_lng = new google.maps.LatLng(gon.profs[i].address.lat, gon.profs[i].address.lng);
+        prof_addresses.push(marker_lat_lng);
+        prof_phones.push(gon.profs[i].phone);
+      }
+      let textNumbers = []
+        for (var i = 0; i < prof_addresses.length; i++) {
+          var distance_from_location = google.maps.geometry.spherical.computeDistanceBetween(address, prof_addresses[i]);
+            if (distance_from_location <= radius) {
+               console.log(prof_addresses[i].lat(), prof_addresses[i].lng());
+               textNumbers.push(prof_phones[i]);
+            }
+        }
+        return textNumbers
+ }
 
-     navigator.geolocation.getCurrentPosition(success, failure, opts)
-   }
+  // Google maps
+  // function initMap(currentLocation){
+  //    let map1;
+  //    map1 = new google.maps.Map(document.getElementById('map1'), {
+  //      center: new google.maps.LatLng(currentLocation),
+  //      zoom: 10
+  //    });
+  //    let markers;
+  //    if (gon.friendBook){
+  //      gon.friendBook.map(loc => {
+  //       locations = {
+  //          lat: loc.lat,
+  //          lng: loc.lng
+  //        }
+  //        markers = new google.maps.Marker({ position: locations, map: map1 })
+  //      })
+  //    }
+  //   }
--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,5 +1,6 @@
 class NotificationsController < ApplicationController
   before_action :authenticate_prof!, only: [:mark_as_read, :index]
+  respond_to :js, :json, :html
 
 
   def index
@@ -16,15 +17,17 @@ class NotificationsController < ApplicationController
     acct_id = ENV["TWILIO_ID"]
     acct_token = ENV["TWILIO_TOKEN"]
     @twilio_number = ENV["TWILIO_NUMBER"]
-    @client = Twilio::REST::Client.new(acct_id, acct_token)
-    profs = Prof.last(2)
-    from = "+19543290694"
-    to = "+19545608938"
-    message = @client.messages.create(
-      from: @twilio_number,
-      to: to,
-      body: "Yoopy"
-    )
+    @client = Twilio::REST::Client.new(acct_id, acct_token)    
+    profs = params[:profs]
+      profs.each do |prof|
+      from = "+19543290694"
+      to = "+1#{prof}"
+      message = @client.messages.create(
+        from: @twilio_number,
+        to: to,
+        body: "Yoopy"
+      )
+    end
   end
 
   def notify_favorites
@@ -48,5 +51,6 @@ class NotificationsController < ApplicationController
 
     end
   end
+
 
 end
